eec11206150a70ed89300a21a99f73b31bf6486d
17YlaAgoshcova <ylaostrog@gmail.com>
2020-08-18 14:08:46 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 5 Agoshkova/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5_Agoshkova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            Console.Write("Введите а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите в: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            void maxim(int aa, int bb, out int cc)
+            {
+                if (aa > bb)
+                {
+                    cc = aa;
+                }
+                else
+                {
+                    cc = bb;
+                }
+            }
+
+            maxim(a, b, out c);
+
+            Console.WriteLine($"Максимальное число: {c}");
+            Console.ReadKey();
+
+        }
+    }
+}
